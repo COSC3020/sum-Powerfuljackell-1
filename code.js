1e478015dd89866461d0b579274287e387015536
Powerfuljackell <johnsonjakel@aol.com>
2024-02-01 04:12:42 -0700

    Modified code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function sum(a) {
-    var sum = a[0];
+    var sum = 0; //found error at line 2, using a[0] results in the first value in the array being used rather than 0
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
